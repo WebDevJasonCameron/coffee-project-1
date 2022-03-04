2e0f2df86420d325ac42991b8818f650fe85bb19
Jason Cameron <dev.jasoncameron@gmail.com>
2022-03-03 21:04:28 -0600

    fixed the issue that you could not return back to the entire list of coffee selection

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 // Creates string of each obj, with html tags
 function renderCoffee(coffee) {                                         //   'coffee' will be an object from the array
-    var html = '<div class="coffee">';                                 //   Creates var with the start of the div
+    let html = '<div class="coffee">';                                 //   Creates let with the start of the div
                                                                         // container with the 'coffee' class
     html += '<div class="coffee-name">' + coffee.name + '</div>';                //   Adding to the string with the coffee name
                                                                         // attribute (with more div tags and h1
@@ -15,8 +15,8 @@ function renderCoffee(coffee) {                                         //   'co
 
 // Runs through the array and calls the renderCoffee function per obj
 function renderCoffees(coffees) {
-    var html = '';                                                      //   Creates a string var
-    for(var i = coffees.length - 1; i >= 0; i--) {                      //   Loop through Coffee array backwards
+    let html = '';                                                      //   Creates a string let
+    for(let i = coffees.length - 1; i >= 0; i--) {                      //   Loop through Coffee array backwards
                                                                         // until 'i' reach to 0
         html += renderCoffee(coffees[i]);
     }
@@ -24,25 +24,51 @@ function renderCoffees(coffees) {
 }
 
 
-// Filter Function
+// Roast Filter Function
 function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;                           //   Var that holds user input
-    var filteredCoffees = [];                                           //   New array var as our output
+    e.preventDefault();
 
-    coffees.forEach(function(coffee) {                             //   Loop through original array
-        if (coffee.roast === selectedRoast) {                           //   Only selecting objects that user requests
-            filteredCoffees.push(coffee);                               //   Pushes those objects into our new output
-                                                                        // array
-        }
-    });
-    coffeeContainer.innerHTML = renderCoffees(filteredCoffees);                   //   Running renderCoffees function with new
+    let selectedRoast = roastSelection.value;
+    let filteredCoffees = [];
+
+    if(selectedRoast === 'all'){
+        coffeeContainer.innerHTML = renderCoffees(coffees);
+    } else {
+        coffees.forEach(function (coffee){
+            if (coffee.roast === selectedRoast) {
+                filteredCoffees.push(coffee)
+            }
+            coffeeContainer.innerHTML = renderCoffees(filteredCoffees);
+        });
+    }
+       //   Running renderCoffees function with new
 }                                                                       // output array to get the string of HTML tags
                                                                         // with object info.  Then it is placed into
                                                                         // the 'coffeeContainer' element on the HTML page
 
+
+
+// Name Search Filter Function
+function filterByName(e){
+    e.preventDefault();
+
+    let selectedRoast = roastSelection.value;
+    let filteredCoffees = [];
+
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast) {
+            filteredCoffees.push(coffee);
+
+        }
+    });
+    coffeeContainer.innerHTML = renderCoffees(filteredCoffees);
+
+}
+
+
+
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
+let coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
     {id: 3, name: 'Cinnamon', roast: 'light'},
@@ -59,11 +85,13 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
-// Setting Vars
-var coffeeContainer = document.querySelector('#coffees-container');
-var submitButton = document.querySelector('#submit');           //   Submit: activate the functions with user
+// Setting lets
+let coffeeContainer = document.querySelector('#coffees-container');
+let submitButton = document.querySelector('#submit');           //   Submit: activate the functions with user
                                                                          // info
-var roastSelection = document.querySelector('#roast-selection');//   Getting user roast input
+let roastSelection = document.querySelector('#roast-selection');//   Getting user roast input
+
+let addCoffeeName = document.querySelector('#add-coffee-name')  //   Get user input for name
 
 
 
